e3c9c8b427b06a85c2d19d72a7c18e314bf97c36
NoeLP03 <laraparranoeabraham@gmail.com>
2026-01-12 16:17:56 -0700

    actualizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,13 +43,14 @@ android {
 }
 
 dependencies {
-    implementation (platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation ("com.google.firebase:firebase-auth")
-    implementation ("com.google.firebase:firebase-firestore")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/petstay/app/ActivityRegistroUsuario.java
+++ app/src/main/java/com/petstay/app/ActivityRegistroUsuario.java
@@ -1,5 +1,6 @@
 package com.petstay.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityRegistroUsuario extends AppCompatActivity {
-
 
     private ImageView imgFotoUsuario;
     private EditText editNombreUsuario;
@@ -18,12 +22,20 @@ public class ActivityRegistroUsuario extends AppCompatActivity {
     private EditText editTelefonoUsuario;
     private Button btnRegistrarUsuario;
 
+    // Instancias de Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_registro_usuario);
 
+        // Inicializar Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        // Inicializar Vistas
         imgFotoUsuario = findViewById(R.id.imgFotoUsuario);
         editNombreUsuario = findViewById(R.id.editNombreUsuario);
         editEmailUsuario = findViewById(R.id.editEmailUsuario);
@@ -39,22 +51,46 @@ public class ActivityRegistroUsuario extends AppCompatActivity {
         });
     }
 
-
     private void registrarUsuario() {
-
-        String nombre = editNombreUsuario.getText().toString();
-        String email = editEmailUsuario.getText().toString();
-        String password = editPasswordUsuario.getText().toString();
-        String telefono = editTelefonoUsuario.getText().toString();
+        String nombre = editNombreUsuario.getText().toString().trim();
+        String email = editEmailUsuario.getText().toString().trim();
+        String password = editPasswordUsuario.getText().toString().trim();
+        String telefono = editTelefonoUsuario.getText().toString().trim();
 
         if (nombre.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Por favor, completa los campos obligatorios (Nombre, Email, Contraseña).", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Completa los campos obligatorios", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
-        String mensaje = "¡Usuario " + nombre + " registrado exitosamente!";
-        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+
+                        String id = mAuth.getCurrentUser().getUid();
+
+                        guardarDatosEnFirestore(id, nombre, email, telefono);
+                    } else {
+                        Toast.makeText(ActivityRegistroUsuario.this, "Error al registrar: " +
+                                task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
+    private void guardarDatosEnFirestore(String id, String nombre, String email, String telefono) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("nombre", nombre);
+        map.put("email", email);
+        map.put("telefono", telefono);
+
+        mFirestore.collection("Usuarios").document(id).set(map)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(ActivityRegistroUsuario.this, "Â¡Usuario guardado exitosamente!", Toast.LENGTH_SHORT).show();
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(ActivityRegistroUsuario.this, "Error al guardar datos: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
 }
